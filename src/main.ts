e7fdefbd17634fe84d3e432bc7f9f49b8f733289
Bartomeu Ramis <44461710+TomeuRamis@users.noreply.github.com>
2025-03-09 12:45:41 +0100

    installed ngrx

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import routeConfig from './routes';
+import { provideStore } from '@ngrx/store';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routeConfig),
-  ]
+    provideStore()
+]
 }).catch(err => console.error(err));
